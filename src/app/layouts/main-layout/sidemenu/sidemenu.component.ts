74cbd7587dcbe2dbdd009d04c0e399b4031f235d
CM-Digitalee <clotilde@digitalee.ch>
2020-10-12 14:06:46 +0200

    Update menu call

--- src/app/layouts/main-layout/sidemenu/sidemenu.component.ts
+++ src/app/layouts/main-layout/sidemenu/sidemenu.component.ts
@@ -16,6 +16,8 @@ export class SidemenuComponent implements OnInit {
 
   constructor(private itemsService: ItemsMenuService) {
     this.result$ = itemsService.resolveItems();
+    // tslint:disable-next-line:max-line-length
+    // this.result$ = [{labelTranslationId: 1, iconId: 1, resourceUrl: 'https://streets.realestate', genericLabel: 'tableau de bord'}, {labelTranslationId: 2, iconId: 2, resourceUrl: 'https://streets.realestate', counter: 2, genericLabel: 'punchlist'}, {labelTranslationId: 3, iconId: 2, resourceUrl: 'https://streets.realestate', counter: 7, menus: [{labelTranslationId: 4, resourceUrl: 'https://streets.realestate', counter: 2, genericLabel: 'etats locatifs'}, {labelTranslationId: 5, resourceUrl: 'https://streets.realestate', counter: 2, genericLabel: 'bilan'}, {labelTranslationId: 6, resourceUrl: 'https://streets.realestate', counter: 1, genericLabel: 'P&P'}, {labelTranslationId: 7, resourceUrl: 'https://streets.realestate', counter: 2, genericLabel: 'Contentieux'}], genericLabel: 'automatic control'}, {labelTranslationId: 8, iconId: 3, resourceUrl: 'https://streets.realestate', genericLabel: 'soldes comptables'}, {labelTranslationId: 9, iconId: 3, resourceUrl: 'https://streets.realestate', menus: [{labelTranslationId: 10, resourceUrl: 'https://streets.realestate', genericLabel: 'données locatives'}, {labelTranslationId: 11, resourceUrl: 'https://streets.realestate', genericLabel: 'etat des vacants'}, {labelTranslationId: 12, resourceUrl: 'https://streets.realestate', genericLabel: 'données de référence'}], genericLabel: 'analyses'}];
     console.log(this.result$);
   }
 
