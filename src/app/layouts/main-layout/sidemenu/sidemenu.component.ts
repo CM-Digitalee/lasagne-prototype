70c99f2da2257cc6cda28d44e37cf3d52c3748d0
CM-Digitalee <clotilde@digitalee.ch>
2020-10-12 11:21:02 +0200

    Update headers for testing

--- src/app/layouts/main-layout/sidemenu/sidemenu.component.ts
+++ src/app/layouts/main-layout/sidemenu/sidemenu.component.ts
@@ -1,12 +1,21 @@
 import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ItemsMenuService} from '../../../service/items-menu.service';
+import {KeycloakBearerInterceptor} from 'keycloak-angular';
+import { HTTP_INTERCEPTORS } from '@angular/core';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true
+    }
+  ]
 })
 export class SidemenuComponent implements OnInit {
 
