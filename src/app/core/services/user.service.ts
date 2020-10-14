5751be98d8a1c19cfde4ddc628d9c4031b93e9aa
CM-Digitalee <clotilde@digitalee.ch>
2020-10-14 14:43:00 +0200

    Add Keycloak logout

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs';
 
 import { User } from '../../../app/shared';
 import users from '../../../fake-data/users';
+import {KeycloakService} from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -18,16 +19,22 @@ export class UserService {
     return this._currentUser$.asObservable();
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private keycloakService: KeycloakService) { }
   login(user: User) {
     this._currentUser$.next(user);
     this.router.navigate(['']);
   }
 
   loginKeycloak(keycloak){
-    //@TODO : Add role from keycloak
+    // @TODO : Add role from keycloak
     const user = {id: 1, name: keycloak.username, role: 'AO'} as User;
     this._currentUser$.next(user);
+  }
+  logoutKeycloak(){
+    this.keycloakService.logout().then(() => {
+      this._currentUser$.next(null);
+      this.router.navigate(['login']);
+    });
   }
 
   logout() {
